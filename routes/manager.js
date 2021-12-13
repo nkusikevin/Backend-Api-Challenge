19046af07989729ff2c34a65274fe9a797e8866f
“nkusikevin” <nkusikvnhart@gmail.com>
2021-12-13 23:19:23 +0200

    adding manager login  controller and route

--- routes/manager.js
+++ routes/manager.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { registerManager } = require("../controllers/manager");
+const { registerManager,loginManager } = require("../controllers/manager");
 
 router.route("/").post(registerManager);
-
+router.route("/login").post(loginManager);
 module.exports = router;
